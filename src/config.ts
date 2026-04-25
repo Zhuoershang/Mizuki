dcc85e4013bd7f9f041d88964479f7c96efb5051
Zhuoershang <79759438+Zhuoershang@users.noreply.github.com>
2026-04-26 02:58:47 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -605,8 +605,8 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 
 	// 侧栏组件布局配置
 	components: {
-		left: ["profile", "announcement", "tags", "card-toc"],
-		right: ["site-stats", "calendar", "categories", "music-sidebar"],
+		left: ["profile", "announcement", "categories", "tags", "card-toc"],
+		right: ["site-stats", "calendar", "music-sidebar"],
 		drawer: [
 			"profile",
 			"announcement",
